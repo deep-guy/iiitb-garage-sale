178a743988a786e1f36996214c32213c4869dcd2
Deep Inder Mohan <mohan.deepinder@qgmail.com>
2021-05-17 07:27:47 +0530

    Product details page routed through redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListListReducer,
+    productDetails: productDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
