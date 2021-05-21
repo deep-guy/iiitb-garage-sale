59d8aaa5f4370640fc1e2d4681dfa23c875dc774
Ayush Yadav <ayushyadav.ay42@gmail.com>
2021-05-21 19:38:55 +0530

    poll SCM testing

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
     console.log(`dummy message for demo`);
+    console.log(`dummy message for SCM polling`);
 });
