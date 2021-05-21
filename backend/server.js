a51c5db7aec371e020bbc94f1bfba209612c791d
Ayush Yadav <ayushyadav.ay42@gmail.com>
2021-05-21 17:15:19 +0530

    added dummy message:

--- backend/server.js
+++ backend/server.js
@@ -36,4 +36,5 @@ app.use((err, req, res, next) =>{
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
+    console.log(`dummy message for demo`);
 });
